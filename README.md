e47a345fa14cb555f0c5ca48d110b2ee3da1cfa3
Paul Stuart <pauleyphonic@gmail.com>
2023-02-20 13:57:33 -0800

    add retro mainenance page option

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dumbweb
 Tiny simple stupid server to use for testing when you just need to get an http response
+
+# options
+To show old-skool under maintenance page:
+ * Set environment variable `MAINTENANCE=true`
+ * or use `-maintenance` flag 
--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"embed"
+	"flag"
 	"fmt"
 	"html"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+)
+
+var (
+	//go:embed static/*
+	content embed.FS
 )
 
 func main() {
+	var maintenance bool
+	flag.BoolVar(&maintenance, "maintenance", false, "show maintenance page")
+	flag.Parse()
+
+	if ok, _ := strconv.ParseBool(os.Getenv("MAINTENANCE")); ok {
+		maintenance = true
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -18,9 +35,21 @@ func main() {
 		addr = "0.0.0.0"
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "[%-6s] %q\n", r.Method, html.EscapeString(r.URL.Path))
-	})
+	if maintenance {
+		log.Println("MAINTAIN!")
+		dir, err := fs.Sub(content, "static")
+		if err != nil {
+			log.Fatalf("failed to get static dir: %v", err)
+		}
+		http.Handle("/", http.FileServer(http.FS(dir)))
+		// http.Handle("/", http.FileServer(http.FS(content)))
+		// http.Handle("/", http.StripPrefix("/static/", http.FileServer(http.FS(content))))
+		// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(content))))
+	} else {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "[%-6s] %q\n", r.Method, html.EscapeString(r.URL.Path))
+		})
+	}
 
 	listen := addr + ":" + port
 	log.Println("listening on: ", listen)
